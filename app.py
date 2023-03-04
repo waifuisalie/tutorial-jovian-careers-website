d11c380f7ec2c498afeab41ef83c91a0c8f14ef9
waifuisalie <stefanme48@gmail.com>
2023-03-04 15:31:02 -0300

    again testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return 'Hmmmmm what'
+    return 'is this it?'
 
 if __name__ == '__main__':
     app.run(debug=True)     # so you can use in replit with browser in browser: app.run(host='0.0.0.0', deb)
