f1a3e2a81760d324830b3563bde79976f888d862
waifuisalie <stefanme48@gmail.com>
2023-03-04 18:12:13 -0300

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return 'Hello WOORLD'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)     # so you can use in replit with browser in browser: app.run(host='0.0.0.0', deb)
