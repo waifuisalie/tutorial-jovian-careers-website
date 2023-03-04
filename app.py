26ed351a3e29b4ba9fdd2b3bde7e505b50bcc530
waifuisalie <stefanme48@gmail.com>
2023-03-04 15:38:34 -0300

    checking if I am commiting

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return 'is this it?'
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(debug=True)     # so you can use in replit with browser in browser: app.run(host='0.0.0.0', deb)
