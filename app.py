c580b8f09433cf1caf6c83b0642167d02c81f915
waifuisalie <stefanme48@gmail.com>
2023-03-11 01:27:08 -0300

    Store form application in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 app = Flask(__name__)
@@ -23,10 +23,20 @@ def show_job(id):
     if not data_fetch:
         return "Job not Found", 404
     job = data_fetch[0]
-    print(job['responsabilities'].split('\n'))
     return render_template('jobpage.html', 
                            job=job
                            )
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    data_fetch = load_job_from_db(id)
+    job = data_fetch[0]
+    add_application_to_db(id, data)
+    return render_template('submitted_application.html', 
+                           application = data,
+                           job = job)
+
     
 if __name__ == '__main__':
     app.run(debug=True)     # so you can use in replit with browser in browser: app.run(host='0.0.0.0', deb)
--- database.py
+++ database.py
@@ -24,9 +24,11 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
     with engine.connect() as conn:
-        result = conn.execute(
-            text(f'SELECT * FROM jobs WHERE id = {id}')
-        )
+        query = text('SELECT * FROM jobs WHERE id = :id')
+        result = conn.execute(query, 
+                              {
+                            'id':id
+                              })
         rows = result.all()
 
         check_row = [dict(zip(result.keys(), row)) for row in rows]
@@ -35,3 +37,18 @@ def load_job_from_db(id):
         else:
             return check_row
     
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url) ")
+        
+        conn.execute(query,
+                    {
+                    'job_id':job_id,
+                    'full_name':data['full_name'],
+                    'email':data['email'],
+                    'linkedin_url':data['linkedin_url'],
+                    'education':data['education'],
+                    'work_experience':data['work_experience'],
+                    'resume_url':data['resume_url']
+                    })
+                     
